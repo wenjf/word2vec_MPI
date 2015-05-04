f916dc59f55f90294abca20cafbc5251146d8310
WEN Jianfeng <wenjf@buaa.edu.cn>
2015-05-04 09:12:08 +0800

    new version

--- replace_synonym.cpp
+++ replace_synonym.cpp
@@ -11,9 +11,9 @@
 using namespace std;
 
 struct vocab_word{
-  string word;
-  string cnt;
-  vocab_word(string s1, string s2){
+  char *word;
+  int cnt;
+  vocab_word(char *s1, int s2){
     word = s1;
     cnt = s2;
   }
@@ -25,15 +25,15 @@ const int vocab_hash_size = 10000000;
 int vocab_hash[vocab_hash_size];
 float *syn0;
 unsigned long long vocab_size = 0;
-
+/*
 int getWordHash(string word){
   unsigned long long a, hash =0;
   int len = word.length();
   for( a<0 ; a<len ; a++) hash = hash *257 + word[a];
   hash = hash % vocab_hash_size;
   return hash;
-}
-
+}*/
+//shouldn't not use string to something char usage
 int getWordHash_c(const char *word) {
   unsigned long long a, hash = 0;
   for (a = 0; a < strlen(word); a++) hash = hash * 257 + word[a];
@@ -41,30 +41,32 @@ int getWordHash_c(const char *word) {
   return hash;
 }
 
-int readOneWord(string line)
+int readOneWord(const char *word, const int freq)
 {
-  int word_end = line.find(' ');
-  string word = line.substr(0, word_end);
-  string cnt =  line.substr(word_end+1, line.length() - word_end ).c_str();
-  int its_hash = getWordHash_c(word.c_str());
+  //int word_end = line.find(' ');
+  //string word = line.substr(0, word_end);
+  //string cnt =  line.substr(word_end+1, line.length() - word_end ).c_str();
+  int its_hash = getWordHash_c(word);
   while(vocab_hash[its_hash]!=-1) its_hash = (its_hash + 1) % vocab_hash_size;
   //printf("%s's hash is %d\n", word.c_str(), its_hash);
   //sleep(2);
   vocab_hash[its_hash] = vocab_size;
-  vocab_word word_here(word, cnt);
+  vocab_word word_here(word, freq);
   vocab.push_back(word_here);
   vocab_size++;
   //printf( "word:%s hash:%d cnt:%s vocab:%lld \n", word.c_str(), its_hash, cnt.c_str(), vocab_size);
   return vocab_size;
+  
 }
 
 int getWordCount(char *path){
   for(int a = 0; a < vocab_hash_size; a++ ) vocab_hash[a] = -1;
-  ifstream f_count;
-  f_count.open( path, ifstream::in );
-  string line;
-  while( getline(f_count, line) ){
-    readOneWord(line);
+  FILE *f_count = fopen(path, "w");
+  //f_count.open( path, ifstream::in );
+  char word[50];
+  int cnt;
+  while( fscanf(f_count, "%s %d", word, &cnt) ){
+    readOneWord(word, cnt);
   }
   printf("load word count file successfully!\n");
 }
@@ -111,11 +113,11 @@ void readVector( char *path){
 }
 
 void replace_low(char *path){
-
+	
 }
 
 int main( int argc, char **argv ){
   getWordCount(argv[1]);
-  readVector(argv[2]);
+  //readVector(argv[2]);
 }
 
